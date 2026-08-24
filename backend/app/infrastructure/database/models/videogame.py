5e1cd4284088837a0172dc67c213183411b1f7b6
Agustín Perez <agustinperez444@gmail.com>
2026-08-23 21:48:47 -0300

    model del orm para la entidad videojuego

---
+++ backend/app/infrastructure/database/models/videogame.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.infrastructure.database.base import Base
+
+
+class VideogameORM(Base):
+    __tablename__ = "videogames"
+
+    videogame_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
