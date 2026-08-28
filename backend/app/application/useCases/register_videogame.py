ff38be2cf692740524a936c4231c54b6e2b84869
Emiliano Castilla <403118@sistemas.frc.utn.edu.ar>
2026-08-28 10:19:25 -0300

    tecnicalidad

--- backend/app/application/useCases/register_videogame.py
+++ backend/app/application/useCases/register_videogame.py
@@ -31,7 +31,7 @@ class RegisterVideogame:
             raise InvalidVideogameNameException(videogame_request.name)
         return videogame_request.name.strip()
 
-    # Validar que el videojuego no exista en la base de datos
+    # Validar que el nombre no exista en la base de datos
     def validate_not_exist_videogame(self, cleaned_name: str) -> bool:
         with self.uow as uow:
             existing_videogame = uow.videogame_repo.get_videogame_by_name(cleaned_name.lower())
