719942aeb0a0531fc8c8d0bd87c01aea141bbdf0
Emiliano Castilla <403118@sistemas.frc.utn.edu.ar>
2026-09-04 00:59:13 -0300

    implmentacion en el repositorio y controlador (el orm fue modificado para añadir la eliminacion en cascada de huerfanos cuando se modifica el gameprofile)

--- backend/app/infrastructure/api/controllers/game_profile_controller.py
+++ backend/app/infrastructure/api/controllers/game_profile_controller.py
@@ -2,7 +2,7 @@ from typing import cast
 from fastapi import APIRouter, Depends
 
 from app.application.useCases.create_videogame_profile import CreateVideogameProfile
-from app.application.useCases.update_videogame_profile import UpdateGameProfile
+from app.application.useCases.update_videogame_profile import UpdateVideogameProfile
 from app.infrastructure.api.dependencies.auth import get_current_player_id
 from app.infrastructure.api.dto.create_videogame_profile_request import CreateGameProfileRequest
 from app.infrastructure.api.dto.create_videogame_profile_response import CreateGameProfileResponse
@@ -22,9 +22,10 @@ def create_game_profile(request: CreateGameProfileRequest, player_id: int = Depe
     return response
 
 @router.put("/game_profile_id", response_model=UpdateGameProfileResponse, status_code=200)
-def update_game_profile():
+def update_game_profile(game_profile_id: int, request: UpdateGameProfileRequest, player_id: int = Depends(get_current_player_id)):
     uow = uow_factory()
-    update_game_profile_use_case = UpdateGameProfile(uow)
-    updated_game_profile = update_game_profile_use_case.execute()
-    response = UpdateGameProfileResponse()
+    update_game_profile_use_case = UpdateVideogameProfile(uow)
+    updated_game_profile = update_game_profile_use_case.execute(player_id, game_profile_id, request)
+    profile_id = cast(int, updated_game_profile.game_profile_id)
+    response = UpdateGameProfileResponse(profile_id=profile_id)
     return response
--- backend/app/infrastructure/database/repositories/game_profile_repository_impl.py
+++ backend/app/infrastructure/database/repositories/game_profile_repository_impl.py
@@ -12,9 +12,10 @@ if TYPE_CHECKING:
 class GameProfileRepositoryImpl(IGameProfileRepository):
     def __init__(self, session: Session):
         self._session: Session = session
+
     #TODO agregar el método para crear role_profile
-    def create_game_profile(self, game_profile: 'GameProfile') -> 'GameProfile':
-        orm_game_profile = GameProfileMapper.domain_to_orm(game_profile)
+    def create_game_profile(self, videogame_profile: 'GameProfile') -> 'GameProfile':
+        orm_game_profile = GameProfileMapper.domain_to_orm(videogame_profile)
         merged_orm = self._session.merge(orm_game_profile)
         self._session.flush() # para obtener el game_profile_id generado
         domain_game_profile = GameProfileMapper.orm_to_domain(merged_orm)
@@ -33,5 +34,16 @@ class GameProfileRepositoryImpl(IGameProfileRepository):
         domain_found = GameProfileMapper.orm_to_domain(found) if found else None
         return domain_found
 
-    def update_game_profile(self, game_profile: 'GameProfile') -> 'GameProfile':
-        pass
+    def update_game_profile(self, videogame_profile: 'GameProfile') -> 'GameProfile':
+
+        orm_game_profile = self._session.query(GameProfileORM).filter(GameProfileORM.game_profile_id == videogame_profile.game_profile_id).first()
+        if orm_game_profile:
+            orm_to_update = GameProfileMapper.domain_to_orm(videogame_profile)
+        # merge() compara el estado actual de la base de datos con el objeto que se le pasa y
+        # deduce qué relaciones se agregaron y cuáles se eliminaron.
+        # (se tuvo que agregar el cascade en el game_profile_orm)
+            merged_orm = self._session.merge(orm_to_update)
+            self._session.flush()
+            self._session.refresh(merged_orm)
+            return GameProfileMapper.orm_to_domain(merged_orm)
+        return videogame_profile
