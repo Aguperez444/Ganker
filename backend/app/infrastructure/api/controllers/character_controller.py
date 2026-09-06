e0577647b9c220f923fef1dad3aee9dc55d8176c
Agustín Perez <agustinperez444@gmail.com>
2026-09-05 21:14:09 -0300

    corregida respuesta del endpoint get characters by videogame id

--- backend/app/infrastructure/api/controllers/character_controller.py
+++ backend/app/infrastructure/api/controllers/character_controller.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends
 
 from app.application.useCases.query_characters import QueryCharacters
 from app.infrastructure.api.dependencies.auth import get_current_player_id
-from app.infrastructure.api.dto.get_ranks_response import GetRanksResponse
+from app.infrastructure.api.dto.get_characters_response import GetCharactersResponse
 
 from app.infrastructure.database.unit_of_work.uow_factory import uow_factory
 
 router = APIRouter(prefix="/api/v1/characters")
 
-@router.get("/{videogame_id}", response_model=GetRanksResponse, status_code=200)
+@router.get("/{videogame_id}", response_model=GetCharactersResponse, status_code=200)
 def get_characters_by_videogame_id(videogame_id: int, _player_id: int = Depends(get_current_player_id)):
     # lo del player_id está para que el endpoint esté protegido, pero no se usa en la lógica de este endpoint
     uow = uow_factory()
