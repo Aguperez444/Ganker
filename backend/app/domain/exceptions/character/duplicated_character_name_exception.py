246e06d065fd754fa9996637787c10dc2a9bd633
Agustín Perez <agustinperez444@gmail.com>
2026-09-07 21:07:00 -0300

    corregidos algunos codigos de error http y añadido el decorador abstractmethod en los métodos de una interfaz

--- backend/app/domain/exceptions/character/duplicated_character_name_exception.py
+++ backend/app/domain/exceptions/character/duplicated_character_name_exception.py
@@ -4,4 +4,4 @@ from app.domain.exceptions.domain_exception import DomainException
 class DuplicatedCharacterNameException(DomainException):
     def __init__(self, name: str, videogame_id: int):
         super().__init__(f"Character with name '{name}' already exists in videogame with ID {videogame_id}.",
-                         status_code=400)
+                         status_code=409)
--- backend/app/domain/exceptions/user/unauthorized_exception.py
+++ backend/app/domain/exceptions/user/unauthorized_exception.py
@@ -3,4 +3,4 @@ from app.domain.exceptions.domain_exception import DomainException
 
 class UnauthorizedException(DomainException):
     def __init__(self, user_id: int, user_role: str, attempted_role: str):
-        super().__init__(f"User with ID {user_id} and role {user_role} is not authorized to register a user with role {attempted_role}.", status_code=401)
+        super().__init__(f"User with ID {user_id} and role {user_role} is not authorized to register a user with role {attempted_role}.", status_code=403)
