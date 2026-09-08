a9c0154c5396e8a5afee780058de95e49e58841b
Agustín Perez <agustinperez444@gmail.com>
2026-09-08 14:27:24 -0300

    corregido error en el orm de role profile que impedia crear un perfil con 2 o mas roles

--- backend/app/infrastructure/database/models/role_profile_orm.py
+++ backend/app/infrastructure/database/models/role_profile_orm.py
@@ -14,8 +14,8 @@ if TYPE_CHECKING:
 class RoleProfileORM(Base):
     __tablename__ = "role_profile"
 
-    role_profile_id: Mapped[Optional[int]] = mapped_column(primary_key=True, autoincrement=True)
-    game_profile_id: Mapped[Optional[int]] = mapped_column(ForeignKey("game_profile.game_profile_id"), nullable=False)
+    role_profile_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    game_profile_id: Mapped[int] = mapped_column(ForeignKey("game_profile.game_profile_id"), nullable=False)
     role_id: Mapped[int] = mapped_column(ForeignKey("role.role_id"), nullable=False)
     rank_id: Mapped[int] = mapped_column(ForeignKey("rank.rank_id"), nullable=False)
 
