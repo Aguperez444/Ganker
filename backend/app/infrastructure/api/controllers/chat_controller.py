1684219220efc66bdc62dd0288bb2fdf48ea09d2
Agustín Perez <agustinperez444@gmail.com>
2026-09-22 05:33:35 -0300

    Estandarizada una ruta en un controller, corregido el CamelCase en un DTO

--- backend/app/infrastructure/api/controllers/chat_controller.py
+++ backend/app/infrastructure/api/controllers/chat_controller.py
@@ -10,7 +10,7 @@ from app.application.use_cases.query_conversation import QueryConversations
 from app.infrastructure.api.dependencies.auth import get_current_user_id, require_player
 from app.infrastructure.api.dto.request.start_conversation_request import StartConversationRequest
 from app.infrastructure.api.dto.response.conversation_summary_response import ConversationSummaryResponse
-from app.infrastructure.api.dto.response.create_conversation_summary_response import CreateconversationSummaryResponse
+from app.infrastructure.api.dto.response.create_conversation_summary_response import CreateConversationSummaryResponse
 from app.infrastructure.api.dto.response.get_messages_response import GetMessagesResponse
 from app.infrastructure.api.dto.response.messages_read_notification_response import MessagesReadNotificationResponse
 from app.infrastructure.api.dto.response.notification_type_enum import NotificationType
@@ -19,7 +19,7 @@ from app.infrastructure.api.chat.connection_manager import chat_manager
 
 router = APIRouter(prefix="/api/v1/chat", tags=["Chat"])
 
-@router.post("", response_model=CreateconversationSummaryResponse, status_code=status.HTTP_200_OK, dependencies=[Depends(require_player)])
+@router.post("", response_model=CreateConversationSummaryResponse, status_code=status.HTTP_200_OK, dependencies=[Depends(require_player)])
 def start_or_get_conversation(payload: StartConversationRequest, current_user_id: int = Depends(get_current_user_id)):
 
     uow = uow_factory()
@@ -29,7 +29,7 @@ def start_or_get_conversation(payload: StartConversationRequest, current_user_id
         target_user_id=payload.target_user_id
     )
 
-    return CreateconversationSummaryResponse(
+    return CreateConversationSummaryResponse(
         conversation_id=cast(int, conversation.conversation_id),
         player_1_id=cast(int, conversation.user_1.user_id),
         player_2_id=cast(int, conversation.user_2.user_id)
@@ -44,7 +44,7 @@ def get_my_conversations(current_user_id: int = Depends(get_current_user_id)):
     return query_conversations.by_user_id(current_user_id)
 
 
-@router.get("/conversations/messages/{conversation_id}", response_model=GetMessagesResponse, dependencies=[Depends(require_player)])
+@router.get("/conversations/{conversation_id}/messages", response_model=GetMessagesResponse, dependencies=[Depends(require_player)])
 def get_messages(conversation_id: int, size: int = 30, page: int = 1, current_user_id: int = Depends(get_current_user_id)):
     uow = uow_factory()
     get_messages_use_case = GetMessages(uow)
