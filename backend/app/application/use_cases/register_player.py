9a3cdca7796a423ebd3533dafa5bb60a7dd6af9f
Agustín Perez <agustinperez444@gmail.com>
2026-09-22 18:46:08 -0300

    Update register_player.py

--- backend/app/application/use_cases/register_player.py
+++ backend/app/application/use_cases/register_player.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import re
 
 from typing import TYPE_CHECKING, cast
@@ -43,7 +43,7 @@ class RegisterPlayer:
         hashed_pass = self.pass_hasher.hash_password(player_data.password)
 
         # crear el usuario en el dominio
-        new_player = User(None, player_data.username, player_data.name, player_data.mail, hashed_pass, UserRole.PLAYER, [], None, datetime.datetime.now())
+        new_player = User(None, player_data.username, player_data.name, player_data.mail, hashed_pass, UserRole.PLAYER, [], None, datetime.now())
 
         # persistir el usuario en la base de datos y obtener el usuario registrado con su id
         with self.uow as uow:
